db069ff8f9af3cb43335a89650d66957bdf69e35
luupanu <22541529+luupanu@users.noreply.github.com>
2022-06-16 19:51:22 +0300

    rename stuff, add non-essential formulae

--- setup.sh
+++ setup.sh
@@ -3,22 +3,36 @@
 # Install from configs.
 #
 # Parameters:
-#   ./setup.sh apps  - install MacOS applications
-#   ./setup.sh conda - install miniconda with some good scientific packages
-#   ./setup.sh sc    - install basic audio tools: SuperCollider and Audacity
+#   ./setup.sh common   - install brew and XCode Command Line Tools
+#   ./setup.sh apps     - install MacOS applications
+#   ./setup.sh conda    - install miniconda with some essential scientific packages
+#   ./setup.sh formulae - install non-essential brew formulae
+#   ./setup.sh music    - install basic audio tools such as SuperCollider and Audacity
 
-xcode-select --install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+# check if we have arguments
 if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
+    echo "No arguments supplied."
+    printf "\n"
+    echo "Usage:"
+    echo "  ./setup.sh common   - install brew and XCode Command Line Tools"
+    echo "  ----------------------------------------------------------------------------------"
+    echo "  ./setup.sh apps     - install MacOS applications"
+    echo "  ./setup.sh conda    - install miniconda with some essential scientific packages"
+    echo "  ./setup.sh formulae - install non-essential brew formulae"
+    echo "  ./setup.sh music    - install basic audio tools such as SuperCollider and Audacity"
+    printf "\n"
     exit 1
 fi
 
 # Common installation
-brew bundle --file Brewfile
-cp ".zsh_aliases" "$HOME/.zsh_aliases"
-cp ".zshrc" "$HOME/.zshrc"
+if [ $1 == "common" ]; then
+    xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    brew bundle --file Brewfile
+    cp ".zsh_aliases" "$HOME/.zsh_aliases"
+    cp ".zshrc" "$HOME/.zshrc"
+fi
 
 # apps installation
 if [ $1 == "apps" ]; then
@@ -32,8 +46,14 @@ if [ $1 == "conda" ]; then
     ./setup.sh
 fi
 
-# SuperCollider installation
-if [ $1 == "sc" ]; then
+# formulae installation
+if [ $1 == "formulae" ]; then
+    cd formulae
+    ./setup.sh
+fi
+
+# music installation
+if [ $1 == "music" ]; then
     cd supercollider
     ./setup.sh
 fi

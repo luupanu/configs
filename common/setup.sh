ebec3f2a14f05a7ac1704a2550e36d735497e9d2
luupanu <22541529+luupanu@users.noreply.github.com>
2022-06-17 14:03:54 +0300

    s/NSGlobalDomain/-g

--- common/setup.sh
+++ common/setup.sh
@@ -7,9 +7,12 @@ cp ".zsh_aliases" "$HOME/.zsh_aliases"
 cp ".zshrc" "$HOME/.zshrc"
 
 # apple system preferences
+defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g com.apple.swipescrolldirection -bool NO
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g NSWindowResizeTime -float 0
 defaults write -g QLPanelAnimationDuration -float 0
 defaults write com.apple.controlcenter.plist BatteryShowPercentage -bool true
 defaults write com.apple.controlcenter.plist Bluetooth -int 18
@@ -22,6 +25,4 @@ defaults write com.apple.dock tilesize -int 24
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tr-corner -int 4
 defaults write com.apple.finder DisableAllAnimations -bool true
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-defaults write NSGlobalDomain NSWindowResizeTime -float 0
+

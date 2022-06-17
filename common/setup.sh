029dc2ca39e304efe0be39cc994d2c51c7bc501f
luupanu <22541529+luupanu@users.noreply.github.com>
2022-06-17 14:04:24 +0300

    bool no -> bool false

--- common/setup.sh
+++ common/setup.sh
@@ -8,7 +8,7 @@ cp ".zshrc" "$HOME/.zshrc"
 
 # apple system preferences
 defaults write -g ApplePressAndHoldEnabled -bool false
-defaults write -g com.apple.swipescrolldirection -bool NO
+defaults write -g com.apple.swipescrolldirection -bool false
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
